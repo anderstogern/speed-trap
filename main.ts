4f766d6b53b6f8af5245e2a9a9925387ace24d7a
Anders Sabinsky Tøgern <ast@hoast.dk>
2021-02-23 21:39:30 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -7,25 +7,26 @@ function getDistance () {
     pins.digitalWritePin(DigitalPin.P0, 0)
     return pins.pulseIn(DigitalPin.P1, PulseValue.High) * 0.026881720430108
 }
-function getSpeed () {
-    td = 500000
+function getSpeed (td: number, ts: number) {
     d0 = getDistance()
     control.waitMicros(td)
     d1 = getDistance()
     d = d0 - d1
-    serial.writeValue("d", d)
     // serial.writeValue("d0", d0)
     // serial.writeValue("d1", d1)
-    return d * 0.036 * (1000000 / td)
+    return d * 0.036 * ts
 }
 let timeout = 0
 let v = 0
 let d = 0
 let d1 = 0
 let d0 = 0
+let ts = 0
 let td = 0
+td = 300000
+ts = 1000000 / td
 max7219_7segment.setup(
-1,
+8,
 DigitalPin.P14,
 DigitalPin.P15,
 DigitalPin.P16,
@@ -35,13 +36,17 @@ max7219_7segment.fillAll()
 control.waitMicros(1000000)
 max7219_7segment.clearAll()
 basic.forever(function () {
-    v = getSpeed()
+    serial.writeValue("millis", control.millis())
+    v = getSpeed(td, ts)
     if (v > 2) {
         max7219_7segment.clearAll()
         max7219_7segment.displayNumber(v)
         timeout = control.millis()
+        serial.writeValue("timeout", timeout)
     }
-    if (control.millis() - timeout > 5000) {
+    if (timeout > 0 && control.millis() - timeout > 5000) {
+        serial.writeValue("dtimeout", control.millis() - timeout)
         max7219_7segment.clearAll()
+        timeout = 0
     }
 })
